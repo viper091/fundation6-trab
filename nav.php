07680e4af859567421e2720319b5b9200d65e908
viper091 <vitorperin.emanuel@gmail.com>
2017-11-29 00:01:50 -0200

    finalizado

--- nav.php
+++ nav.php
@@ -18,7 +18,31 @@
             <li><a href="index.php">Home</a></li>
             <li><a href="flightradar.php">FlightRadar24</a></li>
 
-
         </ul>
+    </div>
+    <style>
+        .airs img{
+        height: 60px ; width:130px;
+
+
+        }
+
+    </style>
+    <div class="top-bar-right airs" >
+
+
+                <img src="imgs/logos/boeing.png" >
+
+                <img src="imgs/logos/avianca.png" >
+                <img src="imgs/logos/gol.png" >
+
+
+                <img src="imgs/logos/luft.png" >
+
+                <img src="imgs/logos/emirates.png">
+             <img src="imgs/logos/latam.png" >
+
+
+
     </div>
 </div>
